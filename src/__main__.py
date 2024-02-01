9fea0ef27995fdd05b84c2aa982a14f2510acc1a
oliverpowerboy <oliverd@live.co.uk>
2024-02-01 20:54:06 +0000

    init

---
+++ src/__main__.py
@@ -0,0 +1,8 @@
+# This is where your code should go
+
+def create_trade():
+    pass
+
+
+if __name__ == "__main__.py":
+    create_trade()
---
+++ src/trading_api.py
@@ -0,0 +1,18 @@
+class portfolio:
+    pass
+
+
+class asset:
+    pass
+
+
+class option:
+    pass
+
+
+class call(option):
+    pass
+
+
+class put(option):
+    pass
