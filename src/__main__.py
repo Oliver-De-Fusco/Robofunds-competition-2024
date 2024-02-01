fcb458762c9057756b34d56daeac000adc55f3bf
Oliver <oliverd@live.co.uk>
2024-02-01 21:26:11 +0000

    dev_1

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,10 @@
 # This is where your code should go
+from trading_api import *
+
 
 def create_trade():
-    pass
+    print(option(1,2,3,"pall","buy"))
 
 
-if __name__ == "__main__.py":
+if __name__ == "__main__":
     create_trade()
--- src/trading_api.py
+++ src/trading_api.py
@@ -1,18 +1,41 @@
+import datetime
+
 class portfolio:
-    pass
+    def __init__(self, start_balance) -> None:
+        self.balance = start_balance
+        self.positions = [""]
+        self.trade_history = []
+    
+    
+    def buy(self, asset, type):
+        self.
+
+
+    def sell(self):
+        pass
+
 
 
 class asset:
-    pass
+    def __init__(self, asset, price) -> None:
+        self.asset = asset
+        self.price = price
 
 
 class option:
-    pass
+    def __init__(self, strike, cost, expiry, type, side) -> None:
 
+        self.strike = strike
+        self.cost = cost
 
-class call(option):
-    pass
+        if isinstance(expiry, datetime.datetime):
+            self.expiry = expiry
+        else: raise TypeError("Inappropriate argument type.")
 
+        if type.lower() in ["call", "put"]:
+            self.type = type.lower()
+        else: raise ValueError("Incorrect option type.")
 
-class put(option):
-    pass
+        if side.lower() in ["buy", "sell"]:
+            self.side = side.lower()
+        else: raise ValueError("Incorrect buy/sell side.")
