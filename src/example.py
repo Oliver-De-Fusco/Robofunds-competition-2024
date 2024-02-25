6dd02087770c4e921dac3effb0af33d10e95ea6c
Oliver <oliverd@live.co.uk>
2024-02-25 23:57:18 +0000

    positions rebuilt and black-scholes implementation

--- src/example.py
+++ src/example.py
@@ -1,49 +1,37 @@
-#####################################################
-## ----------------------------------------------- ##
-## LIBF Fintech Society Robofunds Competition 2024 ##
-## ----------------------------------------------- ##
-#####################################################
+#######################################################
+## ------------------------------------------------- ##
+## LIBF Fintech Society - Robofunds Competition 2024 ##
+## ------------------------------------------------- ##
+#######################################################
 
 from trading_api import *
 import datetime
+from dateutil.relativedelta import relativedelta
 import yfinance
 import pandas as pd
 
 def trade(account, market_data, recursive_data=None):
+    msft_asset = asset("MSFT",110, "buy",10)
+    msft_call = optionContract(msft_asset, "call", 120, account.date + relativedelta(months=+3), 0.2, account.risk_Free)
+    order = (msft_call)
 
-    currency_to_trade = "USD", "GBP"
-    # Get current spot price for USD and GBP
-    current_spot_price = market_data["USDGBP=X"].tail(1).values[0]
-    # Create spot purchase order to purchase USD using GBP
-    order = currency_pair(currency_to_trade[0], currency_to_trade[1], rate=current_spot_price, quantity=1000)
-    # Purchase order
-    account.buy(order)
-    # Return data
     return order, recursive_data
 
 
 if __name__ == "__main__":
-    # This if statement is required, all code to run the function must be located under here
+    # This if statement is required, all code to run and test the function must be located under here
 
     
-    # Initialise the portfolio to be traded
+    # Initialise the portfolio and variables for trade function
     portfolio_1 = portfolio(1000)
-    # print(portfolio_1.positions["MSFT"])
-    # print(portfolio_1.positions["MSFT"]["position"])
-    # print(portfolio_1.positions["MSFT"]["contracts"])
+    recursive_data = 0
+    market_data = [None] * 3
 
-    msft_call = optionContract("MSFT", 400, 3, datetime.datetime.now(),"call")
+    # send trade history into function
+    for index, day in enumerate(market_data):
+        orders, recursive_data = trade(portfolio_1, market_data[:index], recursive_data)
 
-    portfolio_1.buy(msft_call, 400)
-    portfolio_1.buy(msft_call,200)
+        portfolio_1.process_Orders(orders)
+        portfolio.date += relativedelta(months=1)
 
-    print(portfolio_1.positions["MSFT"]["contracts"])
-    # print(portfolio_1.positions)c
-
-    """apple = asset("AAPL",122)
-
-    portfolio_1.buy(apple, 500)
-    print(portfolio_1.positions)
-    portfolio_1.sell(apple, 500)
-    print(portfolio_1.positions)
-"""
+    print(portfolio_1.open_Positions)
--- src/trading_api.py
+++ src/trading_api.py
@@ -1,113 +1,110 @@
-#####################################################
-## ----------------------------------------------- ##
-## LIBF Fintech Society Robofunds Competition 2024 ##
-## ----------------------------------------------- ##
-#####################################################
+#######################################################
+## ------------------------------------------------- ##
+## LIBF Fintech Society - Robofunds Competition 2024 ##
+## ------------------------------------------------- ##
+#######################################################
 
 import datetime
+from dateutil.relativedelta import relativedelta
 import numpy
+import scipy.stats as stats
+import math
 from collections import defaultdict
-from dataclasses import dataclass 
+from dataclasses import dataclass
 
-WHITELIST = ("MSFT")
 
 class portfolio:
     date = datetime.datetime.now()
+    risk_Free = 0.05
 
     def __init__(self, balance) -> None:
-        self.positions = defaultdict(lambda :{"position" : 0, "contracts" : defaultdict(int)})
+        # Keeps track of the amount of shares of an asset currently owned
+        self.positions = defaultdict(int)
+        # open positions is a list of tuples: [(contract, price per contract)]
+        self.open_Positions = []
+        # Balance in USD
         self.balance = balance
-        self.trade_history = []
 
-    def buy(self, order, quantity):
-        if isinstance(order, optionContract):
-            # Add option contract to open position
-            order.side = "buy"
-            if order in self.positions[order.asset]["contracts"]:
-                self.positions[order.asset]["contracts"][order] += quantity
-            else:
-                self.positions[order.asset]["contracts"][order] += quantity
+    def process_Orders(self, *orders):
+        for order in orders:
+            if isinstance(order, optionContract):
+                # Process option contracts
+                delta_time = (order.expiry - portfolio.date).days / 255
+                price = black_scholes(order, delta_time)
+                self.open_Positions.append((order, price))
+                balance_Change = order.underlying_Asset.quantity * price
 
+            elif isinstance(order, asset):
+                # Process standard buy/sell
+                self.positions[order.asset] += order.underlying_Asset.quantity
+                balance_Change = order.quantity * price
 
-        else:
-            # Buy asset immediately
-            self.positions[order.asset]["position"] += quantity
-            self.trade_history.append(order)
-        
-        self.balance -= order.cost * quantity
+            if order.underlying_Asset.side == "buy":
+                self.balance += balance_Change
+            elif order.underlying_Asset.side == "sell":
+                self.balance -= balance_Change
 
-    def sell(self, order, quantity):
-        if isinstance(order, optionContract):
-            # Add option contract to open position
-            order.side = "sell"
-            self.positions[order.asset]["contracts"][order] -= quantity
+    def execute_option_contract(self, contract_Tuple, current_spot):
+        """Executes a given contract
 
+        All contracts are settled directly to USD
+        """
+        contract, price = contract_Tuple
 
-        else:
-            # Sell asset immediately
-            self.positions[order.asset]["position"] -= quantity
-            self.trade_history.append(order)
-        
-        self.balance += order.cost * quantity
-
-    def execute_option_contract(self, contract, spot):
-        """Executes a given contract"""
         if not isinstance(contract, optionContract):
-            raise TypeError("Contract must be option class.")
+            raise TypeError("Option contract not provided")
 
         if contract.expiry != portfolio.date:
-            # European options cannot be excerised early.
-            self.positions[contract.asset.buy]["contracts"].remove(
-                contract)
-            return False
+            # All contracts are european and can only be executed on expiry date
+            return None
 
-        revenue = contract.value(spot)
+        if contract.type == "call":
+            moneyness = max(current_spot - contract.strike, 0) - price
+        elif contract.type == "put":
+            moneyness = max(contract.strike - current_spot, 0) - price
 
-        if contract.side == "sell":
-            proft *= -1
+        if contract.side == "buy":
+            balance += moneyness * contract.quantity
+        elif contract.side == "sell":
+            balance -= moneyness * contract.quantity
 
-        self.positions[contract.asset.buy]["contracts"].remove(contract)
-        self.positions[contract.asset.buy]["balance"] += revenue
-        self.trade_history.append(contract)
-
-        return revenue
+        self.open_Positions.remove(contract_Tuple)
 
 
-@dataclass(frozen=True, eq=True)
+# Dataclasses require fields to be filled and be read-only
+@dataclass(frozen=True)
 class asset:
     asset: str
-    cost: float
+    spot_Price: float
+    side: str
+    quantity: int
 
 
+@dataclass(frozen=True)
 class optionContract:
-    def __init__(self, asset, strike, cost, expiry, type)  -> None:
-        """
-        Using option contracts allow for hedging and strategies to be implemented.
 
-        Costs are in USD
-        """
-        self.asset = asset
-        self.strike = strike
-        self.cost = cost
-        self.side = None
+    underlying_Asset: asset
+    type: str
+    strike: float
+    expiry: datetime.datetime
+    volatility: float
+    # Percentage in decimal form 5% -> 0.05
+    risk_Free_Rate: float
 
-        if isinstance(expiry, datetime.datetime):
-            self.expiry = expiry
-        else:
-            raise TypeError("Expiry must be a datetime object.")
 
-        if type.lower() in ["call", "put"]:
-            self.type = type.lower()
-        else:
-            raise ValueError("Incorrect option type.")
+def black_scholes(contract_Order, time_to_expiry):
+    """Calculates black_scholes equation for options"""
 
-    def value(self, spot):
-        """Value when executed at given spot price"""
+    d_1 = (math.log(contract_Order.underlying_Asset.spot_Price/contract_Order.strike) + (contract_Order.risk_Free_Rate + (contract_Order.volatility)/2)*time_to_expiry)/(math.sqrt(contract_Order.volatility*time_to_expiry))
+    d_2 = d_1 - math.sqrt(contract_Order.volatility*time_to_expiry)
 
-        if self.type == "call":
-            revenue = max(spot - self.strike, 0) * self.quantity
-        else:
-            # put
-            revenue = max(self.strike - spot, 0) * self.quantity
+    if contract_Order.type == "call":
+        expected_value = contract_Order.underlying_Asset.spot_Price * stats.norm.cdf(d_1, 0, 1)
+        value_of_not_exercising = contract_Order.strike * math.exp(-contract_Order.risk_Free_Rate * time_to_expiry) * stats.norm.cdf(d_2, 0, 1)
+        return expected_value - value_of_not_exercising
+    
+    elif contract_Order.type == "put":
+        expected_value = contract_Order.underlying_Asset.spot_Price * stats.norm.cdf(-d_1, 0, 1)
+        value_of_not_exercising = contract_Order.strike * math.exp(-contract_Order.risk_Free_Rate * time_to_expiry) * stats.norm.cdf(-d_2, 0, 1)
+        return value_of_not_exercising - expected_value
 
-        return revenue
